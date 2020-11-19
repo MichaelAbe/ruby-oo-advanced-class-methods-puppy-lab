93d221ebaff6662222fa4666b552ec81d27888f6
MichaelAbe <michael.k.abe@gmail.com>
2020-11-18 23:31:00 -0500

    commit tree b2d3a2567ec1d8919a1c45f5ee55443bf66f2628

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+   @@all = []
+    @@name = []
+    def initialize(name)
+        @name = name
+        @@all << self
+        @@name << name
+        save
+    end
+
+    def self.all
+        @@all.uniq
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@name.uniq.delete_if{|name| name == "Snoopy"}
+    end
+
+    def save
+        @@all << self
+    end
+end
